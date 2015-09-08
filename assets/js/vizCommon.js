fbfddd5157ae3570b3b21c73373a959fcf4719ea
Simon Diemert <simon.diemert@gmail.com>
2015-09-08 13:39:44 -0700

    fixed tooltip colors in ratio viz

--- assets/js/vizCommon.js
+++ assets/js/vizCommon.js
@@ -2,6 +2,11 @@
  * Created by sdiemert on 15-09-02.
  */
 
+var colors = {
+    blue : ['#1C2D3E', '#335579', '#4D83BD', '#65B0FF', '#A6D3FF', '#D0E7FF'],
+    yellow : ['#2A1F0E', '#71521F', '#926927', '#D99B37', '#FDB53F', "#FECE7A"]
+};
+
 var graphOffsets = {top: 20};
 
 // shared fonts
@@ -11,9 +16,10 @@ var fonts = {axisTickSize: 12, axisTitleSize: 14, legendSize: 14, pieMiddle:24};
 //
 
 // ratio color palette
-var colorMap = {clinician: '#D99B37', group: '#4D83BD', network: '#1C2D3E'};
-var colorMap2 = {clinician: '#FDB53F', group: '#65B0FF', network: '#4D83BD'};
-var inverseColorMap = {clinician: '#D0E7FF', group: "#D0E7FF", network: "#D0E7FF"};
+var colorMap = {clinician: colors.yellow[3], group: colors.blue[2], network: colors.blue[0]};
+var colorMap2 = {clinician: colors.yellow[5], group: colors.blue[3], network: colors.blue[1]};
+var fontColorMap = {clinician : "#000000", group : "#000000", network : "#ffffff"};
+var inverseColorMap = {clinician: colors.blue[5], group: colors.blue[5], network:colors.blue[5]};
 
 // demographics pyramid
 var pyramidTooltipColorMap = {male: '#4D83BD', female: '#D99B37', undifferentiated: '#D0E7FF', undefined:'#FDB53F'};
